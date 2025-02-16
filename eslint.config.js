202aff15a24003f84a2cb265c6e7276c1f3f0076
negar kafi <negark9776@gmail.com>
2025-02-16 20:40:29 +0330

    feat: enhance project configuration and update README for Task Manager

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,21 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import { init } from '@fullstacksjs/eslint-config';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default init(
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    esm: true,
+    typescript: {
+      projects: ['tsconfig.json', 'tsconfig.app.json', 'tsconfig.node.json'],
     },
+    strict: true,
+  },
+  {
     plugins: {
-      'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'tailwindcss/no-custom-classname': 'off',
     },
   },
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    checker({ typescript: true }),
+    tsconfigPaths(),
+    react(),
+    svgr(),
+  ],
+  server: {
+    open: true,
+    host: "0.0.0.0",
+  },
+  envPrefix: "APP_",
+});
